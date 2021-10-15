05945301dea16e3e38611ab1f70840646c7b0d2c
iamtooboring1234 <kitty0725gaming@gmail.com>
2021-10-15 17:29:35 +0800

    a

---
+++ Krous_Ex/ForumEntry.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Krous_Ex
+{
+    public partial class ForumEntry : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool insertForum()
+        {
+            Guid ForumGUID = Guid.NewGuid();
+
+            string ForumCategory;
+
+            //string Username = clsLogin.GetLoginUserName;
+
+            if (rdExisting.Checked == true)
+                ForumCategory = ddlCategory.SelectedValue;
+            else
+                ForumCategory = txtNewCategory.Text;
+
+            try
+            {
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Krous_Ex"].ConnectionString);
+                con.Open();
+
+                SqlCommand InsertCommand = new SqlCommand("INSERT INTO FORUM VALUES(@ForumGUID,@StaffGUID,@ForumTopic,@ForumDesc,@ForumCategory,@ForumStatus,@ForumCreatedDate,@ForumCreatedBy,@ForumLastUpdatedDate,@ForumLastUpdatedBy)", con);
+
+                InsertCommand.Parameters.AddWithValue("@ForumGUID", ForumGUID);
+                InsertCommand.Parameters.AddWithValue("@ForumTopic", txrForumTopic.Text);
+                InsertCommand.Parameters.AddWithValue("@ForumDesc", txtForumDesc.Text);
+                InsertCommand.Parameters.AddWithValue("@ForumCategory", ForumCategory);
+                InsertCommand.Parameters.AddWithValue("@ForumStatus", ddlForumStatus.SelectedValue);
+                InsertCommand.Parameters.AddWithValue("@ForumCreatedBy", "Admin");
+                InsertCommand.Parameters.AddWithValue("@ForumCreatedDate", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                InsertCommand.Parameters.AddWithValue("@ForumLastUpdatedBy", "Admin");
+                InsertCommand.Parameters.AddWithValue("@ForumLastUpdatedDate", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+                InsertCommand.ExecuteNonQuery();
+
+                con.Close();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex);
+                return false;
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (insertForum())
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "none", "ShowPopup();", true);
+                Response.Redirect("FAQEntry");
+            }
+            else
+            {
+                clsFunction.DisplayAJAXMessage(this, "Unable to insert. Failed to create.");
+            }
+            
+        }
+
+        protected void rdExisting_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdExisting.Checked == true)
+            {
+                txtNewCategory.Text = "";
+                txtNewCategory.Enabled = false;
+                ddlCategory.Enabled = true;
+            }
+        }
+
+        protected void rdNew_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdNew.Checked == true)
+            {
+                ddlCategory.SelectedValue = "";
+                ddlCategory.Enabled = false;
+                txtNewCategory.Enabled = true;
+            }
+        }
+    }
+}
---
+++ Krous_Ex/clsLogin.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace Krous_Ex
+{
+    public class clsLogin
+    {
+        class SurroundingClass
+        {
+            //public static string GetLoginUserGUID()
+            //{
+            //    string LoginUserGUID = "";
+
+            //    try
+            //    {
+            //        HttpCookie authCookie;
+            //        authCookie = HttpContext.Current.Session[FormsAuthentication.FormsCookieName];
+
+            //        FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(authCookie.Value);
+
+            //        if (ticket != null)
+            //            LoginUserGUID = ticket.Name;
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        LoginUserGUID = "";
+            //    }
+            //    return LoginUserGUID;
+            //}
+
+            public static string GetLoginUserType()
+            {
+                string LoginUserType = "";
+
+                try
+                {
+                    HttpCookie authCookie;
+                    authCookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+
+                    FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(authCookie.Value);
+
+                    if (ticket != null)
+                        LoginUserType = ticket.UserData.ToString();
+                }
+                catch (Exception ex)
+                {
+                    LoginUserType = "";
+                }
+                return LoginUserType;
+            }
+
+            public static string GetLoginUserName()
+            {
+                try
+                {
+                    string UserType;
+
+                    if (GetLoginUserType() != "Citizen")
+                        UserType = "Staff";
+                    else
+                        UserType = "Citizen";
+
+                    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Krous_Ex"].ConnectionString);
+                    con.Open();
+                    SqlCommand GetCommand = new SqlCommand("SELECT " + UserType + "Username FROM " + UserType + " WHERE " + UserType + "GUID = '" + GetLoginUserGUID() + "'", con);
+                    SqlDataReader reader = GetCommand.ExecuteReader();
+
+                    DataTable dtUser = new DataTable();
+                    dtUser.Load(reader);
+                    con.Close();
+
+                    if (dtUser.Rows.Count != 0)
+                        return dtUser.Rows(0).Item("" + UserType + "Username").ToString;
+                    else
+                        return "";
+                }
+                catch (Exception ex)
+                {
+                    return "";
+                }
+            }
+        }
+
+    }
+}
